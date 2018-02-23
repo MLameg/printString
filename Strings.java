6ab9632074bbba8fdf7e1dbf557ae95f6321c8e8
MLameg <MLameg12@gmail.com>
2018-02-23 09:12:18 -0500

    Add files via upload

---
+++ Strings.java
@@ -0,0 +1,16 @@
+public class Strings {
+	public static void main(String[] args) {
+		
+		int myInt = 28;
+		
+		String text = "Hello";
+		String blank = " ";
+		String name = "Herman";
+		String sayhelloname = text + blank + name;
+		
+		System.out.println(text);
+		System.out.println(sayhelloname);
+		System.out.println("Good Evening" + "  " + "Herman");
+		System.out.println("My Integer is  " + myInt);
+	}
+}
